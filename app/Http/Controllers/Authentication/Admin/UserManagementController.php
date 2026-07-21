5a3e42fe7a70c2f44eba9b8f0c23e090b30f92e5
Asma-Saleh-AlAjili <asmasaleh.dev@gmail.com>
2026-07-21 16:13:11 +0200

    feat: add user search feature in UserController

--- app/Http/Controllers/Authentication/Admin/UserManagementController.php
+++ app/Http/Controllers/Authentication/Admin/UserManagementController.php
@@ -6,10 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\BanUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+
 class UserManagementController extends Controller
 {
 
- private function successResponse($resource = null, string $message = "Success", int $code = 200)
+    private function successResponse($resource = null, string $message = "Success", int $code = 200)
     {
         return response()->json([
             'status' => 'success',
@@ -26,30 +27,7 @@ class UserManagementController extends Controller
         ], $code);
     }
 
-    public function index()
-    {
-    //     $users = User::latest()->paginate(20);
-    //     return $this->successResponse(UserResource::collection($users));$users = User::latest()->paginate(20);
 
-    // return $this->successResponse([
-    //     'users' => UserResource::collection($users)->collection,
-    //     'meta' => [
-    //         'current_page' => $users->currentPage(),
-    //         'last_page' => $users->lastPage(),
-    //         'per_page' => $users->perPage(),
-    //         'total' => $users->total(),
-    //     ],
-    // ]);
-
-    $users = User::latest()->paginate(20);
-        return UserResource::collection($users);
-    }
-
-    public function show(User $user)
-    {
-        return $this->successResponse(new UserResource($user));
-    }
-    
     public function ban(BanUserRequest $request, User $user)
     {
         if ($user->id === $request->user()->id) {
@@ -60,9 +38,8 @@ class UserManagementController extends Controller
             return $this->errorResponse('You cannot ban another admin', 403);
         }
 
-        if ($user->is_banned) 
-        {
-        return $this->errorResponse('User is already banned', 422);
+        if ($user->is_banned) {
+            return $this->errorResponse('User is already banned', 422);
         }
 
         $data = $request->validated();
@@ -78,11 +55,10 @@ class UserManagementController extends Controller
         return $this->successResponse(new UserResource($user), 'User banned successfully');
     }
 
-      public function unban(User $user)
+    public function unban(User $user)
     {
-        if (!$user->is_banned) 
-        {
-        return $this->errorResponse('User is not banned', 422);
+        if (!$user->is_banned) {
+            return $this->errorResponse('User is not banned', 422);
         }
 
         $user->update([
